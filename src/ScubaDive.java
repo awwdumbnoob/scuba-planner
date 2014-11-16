75e1b39602af0aa206809958b119be6b0a91fae4
jiajie2011 <jiajie2011@gmail.com>
2014-11-16 10:50:24 +0000

    Change from flowpane to gridpane

--- src/ScubaDive.java
+++ src/ScubaDive.java
@@ -12,16 +12,19 @@ import javafx.stage.Stage;
 import javafx.scene.control.Label;
 import javafx.geometry.Pos;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
 
 public class ScubaDive extends Application 
 {
     
-    FlowPane root = new FlowPane();
+    GridPane root = new GridPane();
     Label eventName = new Label(" Project Name: ");
     Label date = new Label(" Date:      ");
     Label time = new Label(" Time: ");
     Label prototype = new Label(" (THIS IS A PROTOTYPE!)");
+    Label o2 = new Label(" O2 Level:                        ");
+    Label he = new Label(" HE Level:                        ");
+    Label ne = new Label(" NE Level:                        ");
     TextField eventNameField = new TextField();
     TextField fromField = new TextField("MM/DD/YYY");
     TextField fromTime = new TextField("HH:MM");
@@ -29,6 +32,16 @@ public class ScubaDive extends Application
     TextField timeField = new TextField("HH:MM");
     Button gas = new Button("Gas Settings");
     Button save = new Button("Save File");
+    Button done = new Button("Done");
+    Button cancel = new Button("Cancel");
+    ObservableList<String> options1 = FXCollections.observableArrayList("10%", "20%", "30%", "40%", "50%", "60%", "70%", "80%", "90%", "100%");
+    ComboBox optionsList = new ComboBox(options1);
+    ObservableList<String> options2 = FXCollections.observableArrayList("10%", "20%", "30%", "40%", "50%", "60%", "70%", "80%", "90%", "100%");
+    ComboBox optionsList2 = new ComboBox(options2);
+    ObservableList<String> options3 = FXCollections.observableArrayList("10%", "20%", "30%", "40%", "50%", "60%", "70%", "80%", "90%", "100%");
+    ComboBox optionsList3 = new ComboBox(options3);
+    ObservableList<String> options4 = FXCollections.observableArrayList("AM", "PM");
+    ComboBox optionsList4 = new ComboBox(options4);
     
     public static void main(String[] args) 
     {ScubaDive.launch();}  
@@ -39,8 +52,18 @@ public class ScubaDive extends Application
         root.setAlignment(Pos.TOP_LEFT);
         root.setHgap(10);
         root.setVgap(10);
-        root.getChildren().addAll(eventName, eventNameField, date, dateField, time, timeField, gas, save, prototype);
-        Scene scene = new Scene(root, 250, 600);
+        root.add(eventName, 1, 1);
+        root.add(eventNameField, 2, 1);
+        root.add(date, 1, 2);
+        root.add(dateField, 2, 2);
+        root.add(time, 1, 3);
+        root.add(timeField, 2, 3);
+        root.add(optionsList4, 3, 3);
+        root.add(gas, 1, 4);
+        root.add(save, 1, 5);
+        root.add(prototype, 1, 6);
+    
+        Scene scene = new Scene(root, 500, 500);
         primaryStage.setTitle("SCUBA Dive");
         primaryStage.setScene(scene);
         primaryStage.show();
@@ -50,31 +73,23 @@ public class ScubaDive extends Application
         	@Override
         	public void handle(ActionEvent actionEvent)
                 {
-                    FlowPane root = new FlowPane();
-                    Label o2 = new Label(" O2 Level:                        ");
-                    Label he = new Label(" HE Level:                        ");
-                    Label ne = new Label(" NE Level:                        ");
-        	    Button done = new Button("Done");
-        	    Button cancel = new Button("Cancel");
-        	    
-        	    ObservableList<String> options1 = FXCollections.observableArrayList("10%", "20%", "30%", "40%", "50%", "60%", "70%", "80%", "90%", "100%");
-        	    ComboBox repeatsList = new ComboBox(options1);
-        	    
-        	    ObservableList<String> options2 = FXCollections.observableArrayList("10%", "20%", "30%", "40%", "50%", "60%", "70%", "80%", "90%", "100%");
-        	    ComboBox repeatsList2 = new ComboBox(options2);
-        	    
-        	    ObservableList<String> options3 = FXCollections.observableArrayList("10%", "20%", "30%", "40%", "50%", "60%", "70%", "80%", "90%", "100%");
-        	    ComboBox repeatsList3 = new ComboBox(options3);
-        	    
+                    GridPane root = new GridPane();
+                    Scene GasWindow = new Scene(root, 500, 500);
+                    
                     root.setAlignment(Pos.TOP_LEFT);
                     root.setHgap(10);
                     root.setVgap(10);
-                    root.getChildren().addAll(o2, repeatsList, he, repeatsList2, ne, repeatsList3, done, cancel);
-                    Scene repeatScene = new Scene(root, 250, 600);
-                    Stage repeatStage = new Stage();
-                    repeatStage.setTitle("Gas Settings");
-                    repeatStage.setScene(repeatScene);
-                    repeatStage.show();
+                    root.add(o2, 1, 1);
+                    root.add(optionsList, 2, 1);
+                    root.add(he, 1, 2);
+                    root.add(optionsList2, 2, 2);
+                    root.add(ne, 1, 3);
+                    root.add(optionsList3, 2, 3);
+        	    
+                    Stage GasOptions = new Stage();
+                    GasOptions.setTitle("Gas Settings");
+                    GasOptions.setScene(GasWindow);
+                    GasOptions.show();
                 
         	}
         });
