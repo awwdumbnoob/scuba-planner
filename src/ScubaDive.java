9288dd14923d4a11e97d0bf50e73cc66e5a6bedf
jiajie2011 <jiajie2011@gmail.com>
2014-11-18 07:50:58 +0000

    Added preferences, dive settings, and updated gas settings.

--- src/ScubaDive.java
+++ src/ScubaDive.java
@@ -3,16 +3,20 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.ToggleGroup;
-import javafx.stage.Stage;
 import javafx.scene.control.Label;
-import javafx.geometry.Pos;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 public class ScubaDive extends Application 
 {
@@ -22,15 +26,34 @@ public class ScubaDive extends Application
     Label date = new Label(" Date:      ");
     Label time = new Label(" Time: ");
     Label prototype = new Label(" (THIS IS A PROTOTYPE!)");
-    Label o2 = new Label(" O2 Level:                        ");
-    Label he = new Label(" HE Level:                        ");
-    Label ne = new Label(" NE Level:                        ");
+    Label o2 = new Label("O2 Level: ");
+    Label he = new Label("HE Level: ");
+    Label ne = new Label("NE Level: ");
+    Label previousDiveTime = new Label("Previous Dive Time: ");
+    Label surfaceIntervalTime = new Label("Surface Interval Time (SIT): ");
+    Label preferenceUnits = new Label("Units: ");
     TextField eventNameField = new TextField();
     TextField fromField = new TextField("MM/DD/YYY");
     TextField fromTime = new TextField("HH:MM");
     TextField dateField = new TextField("MM/DD/YYY");
     TextField timeField = new TextField("HH:MM");
+    TextField pdt = new TextField("00h00m00s");
+    TextField sit = new TextField("00h00m00s");
     Button gas = new Button("Gas Settings");
+    Button gasOk = new Button ("OK");
+    Button gasCancel = new Button ("Cancel");
+    Button gasApply = new Button ("Apply");
+    Button dive = new Button ("Dive Settings");
+    Button diveImport = new Button ("Import");
+    Button diveOk = new Button ("OK");
+    Button diveCancel = new Button ("Cancel");
+    Button diveApply = new Button ("Apply");
+    Button preferences = new Button ("Preferences");
+    Button preferenceOk = new Button ("OK");
+    Button preferenceCancel = new Button ("Cancel");
+    Button preferenceApply = new Button ("Apply");
+    RadioButton preferenceFeet = new RadioButton("Feet");
+    RadioButton preferenceMeters = new RadioButton("Meter");
     Button save = new Button("Save File");
     Button done = new Button("Done");
     Button cancel = new Button("Cancel");
@@ -49,24 +72,47 @@ public class ScubaDive extends Application
     
     public void start(Stage primaryStage) 
     {
+        Scene scene = new Scene(root, 500, 500);
+        primaryStage.setTitle("Dive Planner (WARNING: THIS IS A PROTOTYPE!)");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        //File Menu
+        MenuBar fileMenu = new MenuBar();
+        Menu file = new Menu("File");
+        MenuItem New = new MenuItem("New");
+        MenuItem Open = new MenuItem("Open");
+        MenuItem Save = new MenuItem("Save");
+        MenuItem saveAs = new MenuItem("Save As");
+        MenuItem Exit = new MenuItem("Exit");
+        Exit.setOnAction(new EventHandler<ActionEvent>() 
+        {
+            public void handle(ActionEvent t) {
+                System.exit(0);
+            }
+        });
+        file.getItems().addAll(New, Open, Save, saveAs, Exit);
+        //Edit Menu
+        Menu edit = new Menu("Edit");
+        Menu view = new Menu("View");
+        Menu help = new Menu("Help");
+        fileMenu.getMenus().addAll(file, edit, view, help);
+        
         root.setAlignment(Pos.TOP_LEFT);
         root.setHgap(10);
         root.setVgap(10);
-        root.add(eventName, 1, 1);
-        root.add(eventNameField, 2, 1);
-        root.add(date, 1, 2);
-        root.add(dateField, 2, 2);
-        root.add(time, 1, 3);
-        root.add(timeField, 2, 3);
-        root.add(optionsList4, 3, 3);
-        root.add(gas, 1, 4);
-        root.add(save, 1, 5);
-        root.add(prototype, 1, 6);
-    
-        Scene scene = new Scene(root, 500, 500);
-        primaryStage.setTitle("SCUBA Dive");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        root.add(fileMenu, 1, 1);
+        root.add(eventName, 1, 2);
+        root.add(eventNameField, 2, 2);
+        root.add(date, 1, 3);
+        root.add(dateField, 2, 3);
+        root.add(time, 1, 4);
+        root.add(timeField, 2, 4);
+        root.add(optionsList4, 3, 4);
+        root.add(gas, 1, 5);
+        root.add(dive, 1, 6);
+        root.add(preferences, 1, 7);
+        root.add(save, 1, 8);
+        root.add(prototype, 1, 9);
         
         gas.setOnAction(new EventHandler<ActionEvent>() 
         {
@@ -74,7 +120,7 @@ public class ScubaDive extends Application
         	public void handle(ActionEvent actionEvent)
                 {
                     GridPane root = new GridPane();
-                    Scene GasWindow = new Scene(root, 500, 500);
+                    Scene GasWindow = new Scene(root, 325, 150);
                     
                     root.setAlignment(Pos.TOP_LEFT);
                     root.setHgap(10);
@@ -85,12 +131,66 @@ public class ScubaDive extends Application
                     root.add(optionsList2, 2, 2);
                     root.add(ne, 1, 3);
                     root.add(optionsList3, 2, 3);
+                    root.add(gasOk, 3, 4);
+                    root.add(gasCancel, 4, 4);
+                    root.add(gasApply, 5, 4);
         	    
                     Stage GasOptions = new Stage();
                     GasOptions.setTitle("Gas Settings");
                     GasOptions.setScene(GasWindow);
                     GasOptions.show();
-                
+        	}
+        });
+        
+        dive.setOnAction(new EventHandler<ActionEvent>() 
+        {
+        	@Override
+        	public void handle(ActionEvent actionEvent)
+                {
+                    GridPane root = new GridPane();
+                    Scene DiveWindow = new Scene(root, 500, 120);
+                    
+                    root.setAlignment(Pos.TOP_LEFT);
+                    root.setHgap(10);
+                    root.setVgap(10);
+                    root.add(previousDiveTime, 1, 1);
+                    root.add(pdt, 2, 1);
+                    root.add(diveImport, 3, 1);
+                    root.add(surfaceIntervalTime, 1, 2);
+                    root.add(sit, 2, 2);
+                    root.add(diveOk, 3, 4);
+                    root.add(diveCancel, 4, 4);
+                    root.add(diveApply, 5, 4);
+        	    
+                    Stage GasOptions = new Stage();
+                    GasOptions.setTitle("Edit Dive Settings");
+                    GasOptions.setScene(DiveWindow);
+                    GasOptions.show();
+        	}
+        });
+        
+        preferences.setOnAction(new EventHandler<ActionEvent>() 
+        {
+        	@Override
+        	public void handle(ActionEvent actionEvent)
+                {
+                    GridPane root = new GridPane();
+                    Scene PreferenceWindow = new Scene(root, 290, 110);
+                    
+                    root.setAlignment(Pos.TOP_LEFT);
+                    root.setHgap(10);
+                    root.setVgap(10);
+                    root.add(preferenceUnits, 1, 1);
+                    root.add(preferenceFeet, 2, 1);
+                    root.add(preferenceMeters, 2, 2);
+                    root.add(preferenceOk, 3, 4);
+                    root.add(preferenceCancel, 4, 4);
+                    root.add(preferenceApply, 5, 4);
+        	    
+                    Stage GasOptions = new Stage();
+                    GasOptions.setTitle("Preferences");
+                    GasOptions.setScene(PreferenceWindow);
+                    GasOptions.show();
         	}
         });
     }
